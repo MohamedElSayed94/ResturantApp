bd55c954ac9074b2e6e33e60850d42140dce1bee
Mohamed Elsayed <mohamed.alsaid.zc@gmail.com>
2019-02-16 10:47:44 +0200

    Initial Commit

---
+++ ResturantApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResturantApp
+//
+//  Created by Mohamed Elsayed on 2/16/19.
+//  Copyright © 2019 Sayedovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
